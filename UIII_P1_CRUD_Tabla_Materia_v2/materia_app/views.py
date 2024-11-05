20c3ea0c3df494524c732e39ea0d4630a0ecaad7
justhin-sanchez-1098 <167554940+justhin-sanchez-1098@users.noreply.github.com>
2024-11-05 13:09:17 -0600

    Add files via upload
    version 2 5-noviembre

---
+++ UIII_P1_CRUD_Tabla_Materia_v2/materia_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from .models import Materia
+# Create your views here.
+
+def inicio_vista(request):
+    lasmaterias = Materia.objects.all()
+    return render(request,"gestionarMateria.html",{"mismaterias":lasmaterias})
+
+def registrarMateria(request):
+    codigo = request.POST["txtcodigo"]
+    nombre = request.POST["txtmateria"]
+    creditos = request.POST["numcreditos"]
+
+    guardarmateria = Materia.objects.create(codigo=codigo,nombre=nombre,creditos=creditos)
+    return redirect("/")
